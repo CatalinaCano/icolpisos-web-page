2c23384c0de3e169e41a7ac8e34ec7cb57cf5a86
Catalina Cano <catalinacano08@hotmail.com>
2023-06-24 00:29:55 +0200

    Se agrega el menu responsive

--- js/app.js
+++ js/app.js
@@ -15,10 +15,7 @@ function animarPresentacion() {
 function animarBarraSuperior() {
   const $barraSuperior = this.document.querySelector("#inicio").children;
   for (const elemento of $barraSuperior) {
-    if (elemento.className === "iconos") {
-      elemento.classList.add("aparecer-izquierda");
-    }
-    elemento.classList.add("aparecer-derecha");
+    elemento.className === "iconos"?elemento.classList.add("aparecer-izquierda"):elemento.classList.add("aparecer-derecha");
   }
 }
 
@@ -26,9 +23,7 @@ function animarBarraSuperior() {
 function animarNavbar() {
   const $navbar = this.document.querySelector("nav").children;
   for (const elemento of $navbar) {
-    if (elemento.localName === "div") {
-      elemento.classList.add("aparecer-izquierda");
-    }
+    elemento.localName === "div"? elemento.classList.add("aparecer-izquierda"):
     elemento.classList.add("aparecer-derecha");
   }
 }
@@ -46,14 +41,13 @@ window.addEventListener("load", function () {
 
 
 
+//Funcion encargada de animar cada una de las secciones al momento que se realice scroll.
 function mostrarSecciones() {
+  const anchoVentana = window.innerHeight;
   const $secciones = document.getElementsByClassName('animar-entrada');
-
-  
   for (let i = 0; i < $secciones.length; i++) {
     let $secccion = $secciones[i];
     let alturaSeccion = $secccion.getBoundingClientRect().top;
-    let anchoVentana = window.innerHeight;
     if (alturaSeccion - anchoVentana <= 0) {
       //Se aplica un operador ternario para hacer que las secciones que están en las posiciones pares aperzcan por la izquierda y las impares por la derecha.
       i%2 == 0 ?  $secccion.classList.add("aparecer-izquierda"): $secccion.classList.add("aparecer-derecha");
@@ -62,4 +56,13 @@ function mostrarSecciones() {
 }
 
 
+
+
 window.addEventListener('scroll', mostrarSecciones);
+
+//EventListener para hacer clic sobre el botón del menu
+const $menu = this.document.getElementById('menu-responsive');
+$menu.addEventListener('click',function(){;
+  const $lista = document.getElementById("lista-nav");
+  $lista.classList.toggle('responsive');
+});
